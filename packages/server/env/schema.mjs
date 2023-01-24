adf1c776295f43a7bdb7e5ef7ab92875ebeb2b9b
Mubin Ansari <mubinansari910@gmail.com>
2023-01-25 00:50:26 +0530

    setup prisma

--- packages/server/env/schema.mjs
+++ packages/server/env/schema.mjs
@@ -7,6 +7,7 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
+  SHADOW_DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
@@ -17,6 +18,7 @@ export const serverSchema = z.object({
  */
 export const serverEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
+  SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
 };
 
