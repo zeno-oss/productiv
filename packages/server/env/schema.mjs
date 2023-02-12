b02bc1659a4e2ee5ef97120b7b11527051324a9a
Mubin Ansari <mubinansari910@gmail.com>
2023-02-13 03:18:20 +0530

    migrate to planetscale for db

--- packages/server/env/schema.mjs
+++ packages/server/env/schema.mjs
@@ -7,7 +7,6 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
-  SHADOW_DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
@@ -18,7 +17,6 @@ export const serverSchema = z.object({
  */
 export const serverEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
-  SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
 };
 
