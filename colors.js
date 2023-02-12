5f15ed741c6ed40bb2d3a2b6bd0d2c153c37685e
Mubin Ansari <mubinansari910@gmail.com>
2023-02-12 16:56:58 +0530

    General: add project colors to common file

--- colors.js
+++ colors.js
@@ -2,14 +2,24 @@
  * Specify your custom tailwind colors here.
  */
 module.exports = {
-  gray: {
-    950: "#18181b",
-  },
-  red: {
-    450: "#fe5f55",
-  },
-  lemon: {
-    200: "#fcf99c",
-    400: "#faf33e",
-  },
+  black: "#141414",
+  white: "#fff",
+  gray: "#9F9F9F",
+  lightGray: "#E3E3E3",
+  banana: "#FCE114",
+  darkBanana: "#E1CB13",
+  turquoise: "#4BEED1",
+  darkTurquoise: "#1AA58C",
+  pictonBlue: "#46D2EF",
+  darkPictonBlue: "#59C7DE",
+  vodka: "#B7ADFF",
+  darkVodka: "#A59CE6",
+  radicalRed: "#F82867",
+  darkRadicalRed: "#E11D5A",
+  coral: "#F5815C",
+  darkCoral: "#E16A4B",
+  dodgerBlue: "#2B8CFB",
+  darkDodgerBlue: "#1E6ED8",
+  plumPurple: "#A949C1",
+  darkPlumPurple: "#8F3DAF",
 };
