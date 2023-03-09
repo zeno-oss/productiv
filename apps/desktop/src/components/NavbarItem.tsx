ad8da634aa67ca720aa87ce66c9802aded1d1f02
Anuj <anujjoshi63@gmail.com>
2023-03-09 21:56:43 +0530

    desktop: improve a11y by button->div in navbarItem

--- apps/desktop/src/components/NavbarItem.tsx
+++ apps/desktop/src/components/NavbarItem.tsx
@@ -1,17 +1,16 @@
 const NavbarItem = ({ text, active }: { text: string; active: boolean }) => {
-  console.log({ active, text });
+  // console.log({ active, text });
   return (
-    <button
+    <div
       className={`flex w-full items-center gap-6 px-4 py-2 ${
         active ? "bg-slate-200" : "hover:bg-slate-100"
       } rounded-lg `}
-      type="button"
     >
       <div className="h-6 w-6 rounded-lg bg-slate-400"></div>
-      <div className={`flex-1 text-left ${active ? "font-medium" : ""}`}>
+      <div className={`flex-1 p-2 text-left ${active ? "font-medium" : ""}`}>
         {text}
       </div>
-    </button>
+    </div>
   );
 };
 
