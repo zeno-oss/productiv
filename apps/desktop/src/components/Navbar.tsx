f06613f5b5b16db60f1af66b78b1cddbf75817bc
Anuj <anujjoshi63@gmail.com>
2023-03-09 21:55:21 +0530

    desktop: add Link to navbar

--- apps/desktop/src/components/Navbar.tsx
+++ apps/desktop/src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import { Link } from "react-router-dom";
 const Navbar: React.FC = () => {
   return (
     <div className="flex items-center gap-4 border-b border-black p-4 text-2xl">
       <HiOutlineMenuAlt2 />
-      <div className="font-bold">Productiv</div>
+      <Link to={"/"}>
+        <div className="select-none font-bold">Productiv</div>
+      </Link>
     </div>
   );
 };
