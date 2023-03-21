b149ce62577ad9757aec7763e921d1b39e8be322
Mubin Ansari <mubinansari910@gmail.com>
2023-03-21 06:43:04 +0530

    General: use dayjs to format date time

--- packages/utils/functions.ts
+++ packages/utils/functions.ts
@@ -1,4 +1,4 @@
-import { MONTHS_NAME } from "variables";
+import dayjs from "dayjs";
 
 export function isMobile() {
   return (
@@ -7,46 +7,22 @@ export function isMobile() {
 }
 
 /**
- * Formats date to DD MMM YYYY, hh:mm am/pm
+ * Formats date to DD MMM YY, hh:mm am/pm
  */
 export const formatDateTime = (date: Date) => {
-  return `${formatDate(date)}, ${formatTime(date)}`;
+  return dayjs(date).format("DD MMM YY, hh:mm A");
 };
 
 /**
- * Formats date to DD MMM YYYY
+ * Formats date to DD MMM YY
  */
 export const formatDate = (date: Date) => {
-  const day = date.getDate();
-  // Add leading zero to day
-  const dayString = day.toString().length === 1 ? `0${day}` : day;
-
-  // Get last two digits of year
-  const year = date.getFullYear().toString().slice(-2);
-
-  const month = MONTHS_NAME[date.getMonth()]; // get month in MMM format
-
-  return `${dayString} ${month} ${year}`;
+  return dayjs(date).format("DD MMM YY");
 };
 
 /**
  * Formats date to hh:mm am/pm
  */
 export const formatTime = (date: Date) => {
-  let hour = date.getHours();
-
-  const period = hour < 12 ? "am" : "pm"; // Set AM/PM
-
-  // Convert 24 hour to 12 hour
-  hour = hour % 12 || 12;
-  // Add leading zero to hours
-  const hourString = hour.toString().length === 1 ? `0${hour}` : hour;
-
-  // Add leading zero to minutes
-  const minute =
-    date.getMinutes().toString().length === 1
-      ? `0${date.getMinutes()}`
-      : date.getMinutes();
-
-  return `${hourString}:${minute} ${period}`;
+  return dayjs(date).format("hh:mm A");
 };
